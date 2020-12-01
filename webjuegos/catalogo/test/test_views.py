576015af25465dc947cad8fbc992bb460d676909
unknown <seba.olivaresa@alumnos.duoc.cl>
2020-11-30 21:48:14 -0300

    Implementando test views

--- webjuegos/catalogo/test/test_views.py
+++ webjuegos/catalogo/test/test_views.py
@@ -4,12 +4,18 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 from catalogo.models import Juego
 
-"""
 class SimpelTest(TestCase):
 
 
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('juegos'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'juegos/juego_list.html')
-"""
+        self.assertTemplateUsed(response, 'catalogo/juego_list.html')
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalogo/videos/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('videos'))
+        self.assertEqual(response.status_code, 200)
